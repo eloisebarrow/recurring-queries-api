df6f49f977157779ad54feaf00f1fa6511a2abc6
Eloise Barrow <eloise@chartbeat.com>
2020-07-28 20:45:47 -0400

    remove react readme info, add starter app.js info

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recurring Queries</h1>
+      <h3>Manage your recurring queries below</h3>
     </div>
   );
 }
