01108e09b3e1d020bdbcf998f922445a50cf3d59
Eloise Barrow <eloise@chartbeat.com>
2020-07-31 18:39:50 -0400

    construct cancel recurring query api call

--- src/services/api-helper.js
+++ src/services/api-helper.js
@@ -1,12 +1,25 @@
 const axios = require('axios');
 const myApiKey = process.env.REACT_APP_CB_API_KEY;
-const api = axios.create({
+const apiList = axios.create({
     baseURL: `https://dashapi.chartbeat.com/query/v2/recurring/list`
+})
+
+const apiCancel = axios.create({
+    baseURL: `https://chartbeat.com/query/v2/recurring/cancel`
 })
 
 export const getQueries = async (host, apiKey) => {
     try {
-        const resp = await api.get(`/?host=${host}&apikey=${apiKey}`)
+        const resp = await apiList.get(`/?host=${host}&apikey=${apiKey}`)
+        return resp.data;
+    } catch (e) {
+        return { error: e.message }
+    }
+}
+
+export const getCancelRecurringQueries = async (host, apiKey, queryId) => {
+    try {
+        const resp = await apiCancel.get(`/?apikey=${apiKey}&host=${host}&query_id=${queryId}`)
         return resp.data;
     } catch (e) {
         return { error: e.message }
