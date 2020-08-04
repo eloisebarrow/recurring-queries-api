1d52f363e3ba379979bea3d7fc9ef71c155f7633
Eloise Barrow <eloise@chartbeat.com>
2020-08-04 10:00:38 -0400

    separate unix ts conversion work into branch

--- src/components/DisplayQueries.jsx
+++ src/components/DisplayQueries.jsx
@@ -45,7 +45,9 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import { getUnixTs } from '../services/api-helper';
+// import { getUnixTs } from '../services/api-helper';
+
+const moment = require('moment'); // moment library to convert unix timestamps
 
 const useRowStyles = makeStyles({
   root: {
@@ -54,6 +56,12 @@ const useRowStyles = makeStyles({
     },
   },
 });
+
+const convertTs = (ts) => {
+  let timestamp = moment.unix(ts)
+  console.log(timestamp)
+  return timestamp._d;
+}
 
 function Row(props) {
   const { row } = props; // each row exists in props, with all its data
