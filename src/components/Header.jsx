a698ce2b064c17a1607b9e5dfe68c03f9599119c
Eloise Barrow <eloise@chartbeat.com>
2020-08-06 22:51:17 -0400

    resize logo, flex header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../App.css'
 
 export default function Header() {
     return (
-        <div>
+        <div className="header-container">
             <h2 className="headline">Manage recurring queries</h2>
+            <img 
+                src={require("../assets/cb-logo-blue.png")} 
+                alt="Chartbeat logo"
+                className="cb-logo-header"></img>
         </div>
     )
 }
