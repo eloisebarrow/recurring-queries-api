897eb7b1d5c941b57c3dabfee8607270e705eb78
Eloise Barrow <eloise@chartbeat.com>
2020-08-07 10:45:47 -0400

    modal styles, wip conditional render

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import '../App.css'
 
 export default function Modal() {
     return (
         <div className="modal-container">
             <div className="modal">
-                <h2>Delete Recurring Query</h2>
-                <p>You're about to delete query...</p>
-                <p>Are you sure you would like to do this?</p>
-                <button>Cancel</button>
-                <button>Delete</button>
+                <header>
+                    <h2>Delete Recurring Query</h2>
+                </header>
+                <section>
+                    <section className="delete-text">
+                        <p>You're about to delete this query.</p>
+                        <p>Are you sure you would like to do this?</p>
+                    </section>
+                    <button className="action-cancel">Cancel</button>
+                    <button className="action-delete">Delete</button>
+                </section>
             </div>
         </div>
     )
