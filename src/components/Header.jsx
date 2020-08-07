1f60a49d291c8cf0673b1b0882117bae2d5c9583
Eloise Barrow <eloise@chartbeat.com>
2020-08-06 23:43:22 -0400

    add link to cb logo and cursor styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,15 @@ export default function Header() {
     return (
         <div className="header-container">
             <h2 className="headline">Manage recurring queries</h2>
-            <img 
-                src={require("../assets/cb-logo-blue.png")} 
-                alt="Chartbeat logo"
-                className="cb-logo-header"></img>
+            <a 
+              href="https://chartbeat.com/publishing/settings/api-keys/" 
+              target="_blank" 
+              rel="noopener noreferrer">
+                <img 
+                    src={require("../assets/cb-logo-blue.png")} 
+                    alt="Chartbeat logo"
+                    className="cb-logo-header"></img>
+            </a>
         </div>
     )
 }
