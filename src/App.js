d7bc5f5e5846f187b6cc5f7a0c1feb58fbfc27bc
Eloise Barrow <eloise@chartbeat.com>
2020-08-17 16:38:14 -0400

    remove useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import { getQueries, getCancelRecurringQueries } from './services/api-helper.js';
 
